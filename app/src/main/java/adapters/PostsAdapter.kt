9dfb1bcdc5d710fbb3cf3e441a8fb898c41bab7c
Jonas <jonas.marini.tot@gmail.com>
2021-07-02 00:12:55 -0300

    editando postagens

---
+++ app/src/main/java/adapters/PostsAdapter.kt
@@ -0,0 +1,157 @@
+package adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.desafio_boticario.R
+import de.hdodenhof.circleimageview.CircleImageView
+import entities.PostsEntity
+import utils.formatDateToString
+
+class PostsAdapter(private val posts: ArrayList<PostsEntity>, private val context: Context,
+                   private var recyclerView: RecyclerView, private var userId: Long): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        const val VIEW_ITEM = 1
+        const val VIEW_PROG = 0
+    }
+
+    private var visibleThreshold = 6
+    private var lastVisibleItem = 0
+    private var totalItemCount = 0
+    private var loading = false
+
+    private var listener: PostsListener? = null
+
+    init {
+        if (recyclerView.layoutManager is androidx.recyclerview.widget.LinearLayoutManager) {
+            val linearLayoutManager = recyclerView.layoutManager as androidx.recyclerview.widget.LinearLayoutManager
+
+            recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    totalItemCount = linearLayoutManager.itemCount
+                    lastVisibleItem = linearLayoutManager.findLastVisibleItemPosition()
+
+                    if (!loading && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+                        if (listener != null) {
+                            listener!!.onLoadMore()
+                        }
+                        loading = true
+                    }
+                }
+            })
+        }
+    }
+
+    override fun onViewRecycled(holder: RecyclerView.ViewHolder) {
+        super.onViewRecycled(holder)
+
+        if (recyclerView.layoutManager is androidx.recyclerview.widget.LinearLayoutManager) {
+            val linearLayoutManager = recyclerView.layoutManager as androidx.recyclerview.widget.LinearLayoutManager
+            recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    totalItemCount = linearLayoutManager.itemCount
+                    lastVisibleItem = linearLayoutManager.findLastVisibleItemPosition()
+
+                    if (!loading && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+                        if (listener != null) {
+                            listener!!.onLoadMore()
+                        }
+                        loading = true
+                    }
+                }
+            })
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (posts[position] != null) VIEW_ITEM else VIEW_PROG
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val viewHolder: RecyclerView.ViewHolder?
+        if (viewType == VIEW_ITEM) {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.posts_item, parent, false)
+            viewHolder = PostsViewHolder(view, parent.context, listener)
+        } else {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.progressbar_item_table_row, parent, false)
+            viewHolder = ProgressViewHolder(view)
+        }
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as PostsViewHolder).bindView(posts[position], context, userId)
+        if (holder is PostsViewHolder) {
+            val post = posts[position]
+            if (post != null) {
+                holder.bindView(post, context, userId)
+            }
+        } else {
+            (holder as ProgressViewHolder).progressBar.isIndeterminate = true
+        }
+    }
+
+    fun setLoaded() {
+        loading = false
+    }
+
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+
+    interface PostsListener {
+        fun onButtonClicked(position: Int)
+        fun onLoadMore()
+    }
+
+    fun setListener(listener: PostsListener) {
+        this.listener = listener
+    }
+
+    class PostsViewHolder(itemView: View, private val context: Context, private val listener: PostsListener?): RecyclerView.ViewHolder(itemView){
+        fun bindView(post: PostsEntity, context: Context, userId: Long){
+
+            val author = itemView.findViewById<TextView>(R.id.txt_author_news_boticario)
+            val image = itemView.findViewById<CircleImageView>(R.id.img_news_boticario)
+            val date = itemView.findViewById<TextView>(R.id.txt_date_news_boticario)
+            val message = itemView.findViewById<TextView>(R.id.txt_message_news_boticario)
+            val imgEdit = itemView.findViewById<ImageView>(R.id.img_edit_post)
+
+            if(post.userId == userId){
+                imgEdit.visibility = View.VISIBLE
+                imgEdit.setOnClickListener {
+                    listener?.onButtonClicked(adapterPosition)
+                }
+            } else {
+                imgEdit.visibility = View.GONE
+            }
+
+            author.text = post.author
+
+            Glide
+                .with(context)
+                .load(R.drawable.logo_flower)
+                .into(image)
+
+            message.text = post.message
+            date.text = post.created_date?.formatDateToString()
+
+        }
+    }
+
+    class ProgressViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var progressBar: ProgressBar = itemView.findViewById(R.id.pb_load_more)
+
+    }
+}
--- app/src/main/java/fragments/posts/PostsFragment.kt
+++ app/src/main/java/fragments/posts/PostsFragment.kt
@@ -1,13 +1,44 @@
 package fragments.posts
 
+import adapters.PostsAdapter
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.desafio_boticario.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.orm.query.Condition
+import com.orm.query.Select
+import constants.Constants
+import entities.PostsEntity
+import entities.UserEntity
+import java.util.*
 
 class PostsFragment : Fragment() {
+
+    private var adapter: PostsAdapter? = null
+    private var listPosts = ArrayList<PostsEntity>()
+
+    lateinit var rvPosts: RecyclerView
+    lateinit var pbPosts: ProgressBar
+    lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
+    var userLogged: UserEntity? = null
+
+    private var offset = 0
+    private val loadPerPage = 15
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_posts, container, false)
@@ -15,5 +46,158 @@ class PostsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        getUserLogged()
+
+        pbPosts = view.findViewById(R.id.pb_posts)
+        swipeRefreshLayout = view.findViewById(R.id.srl_posts)
+
+        swipeRefreshLayout.setColorSchemeResources(R.color.dark_purple)
+        swipeRefreshLayout.setOnRefreshListener {
+            offset = 0
+            listPosts.clear()
+            loadPosts()
+        }
+        swipeRefreshLayout.isEnabled = true
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab_add_post)
+        fab.setOnClickListener {
+            showDialogNewPost()
+        }
+
+        rvPosts = view.findViewById(R.id.rv_posts)
+        rvPosts.layoutManager = LinearLayoutManager(requireContext())
+        rvPosts.itemAnimator = DefaultItemAnimator()
+
+        context?.let {
+            userLogged?.let { user ->
+                adapter = PostsAdapter(listPosts, it, rvPosts, user.id)
+                rvPosts.adapter = adapter
+            }
+        }
+
+        adapter?.setListener(object : PostsAdapter.PostsListener {
+
+            override fun onButtonClicked(position: Int) {
+                val postEdit = listPosts[position]
+                showDialogEditPost(postEdit)
+            }
+
+            override fun onLoadMore() {
+                loadPosts()
+            }
+        })
+    }
+
+    private fun getUserLogged(){
+
+        val sharedPreferences = androidx.preference.PreferenceManager.getDefaultSharedPreferences(context)
+        val userId = sharedPreferences.getLong(Constants.USER_LOGGED_ID, 0)
+
+        userLogged = Select.from(UserEntity::class.java)
+            .where(Condition.prop("id").eq(userId))
+            .first()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadPosts()
+    }
+
+    private fun loadPosts(){
+
+        val posts = Select.from(PostsEntity::class.java)
+            .limit("$offset,$loadPerPage")
+            .orderBy("id")
+            .list()
+
+        if(!posts.isNullOrEmpty()){
+            listPosts.addAll(posts)
+            adapter?.notifyDataSetChanged()
+            offset += posts.size
+
+            adapter?.setLoaded()
+        }
+
+        pbPosts.visibility = View.GONE
+
+        if (swipeRefreshLayout.isRefreshing) {
+            swipeRefreshLayout.isRefreshing = false
+        }
+    }
+
+    private fun showDialogEditPost(editPostEntity: PostsEntity){
+
+        val viewDialog = Dialog(requireContext())
+        viewDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        viewDialog.setCancelable(true)
+        viewDialog.setContentView(R.layout.dialog_new_post)
+        viewDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val title = viewDialog.findViewById<TextView>(R.id.txt_dialog)
+        val editPost = viewDialog.findViewById<EditText>(R.id.edit_new_post)
+        val btConfirm = viewDialog.findViewById<TextView>(R.id.bt_confirm_new_post)
+        val btCancel = viewDialog.findViewById<TextView>(R.id.bt_cancel_new_post)
+
+        editPost.setText(editPostEntity.message)
+
+        title.text = getString(R.string.edit_post)
+
+        btConfirm.setOnClickListener {
+            if(editPost.text.toString().trim().isNotEmpty()){
+                editPostEntity.message = editPost.text.toString()
+                saveEditPost(editPostEntity)
+                viewDialog.dismiss()
+            }
+        }
+
+        btCancel.setOnClickListener {
+            viewDialog.dismiss()
+        }
+
+        viewDialog.show()
+    }
+
+    private fun showDialogNewPost(){
+
+        val viewDialog = Dialog(requireContext())
+        viewDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        viewDialog.setCancelable(true)
+        viewDialog.setContentView(R.layout.dialog_new_post)
+        viewDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val title = viewDialog.findViewById<TextView>(R.id.txt_dialog)
+        val editNewPost = viewDialog.findViewById<EditText>(R.id.edit_new_post)
+        val btConfirm = viewDialog.findViewById<TextView>(R.id.bt_confirm_new_post)
+        val btCancel = viewDialog.findViewById<TextView>(R.id.bt_cancel_new_post)
+
+        title.text = getString(R.string.new_post)
+
+        btConfirm.setOnClickListener {
+            if(editNewPost.text.toString().trim().isNotEmpty()){
+                saveNewPost(editNewPost.text.toString())
+                viewDialog.dismiss()
+            }
+        }
+
+        btCancel.setOnClickListener {
+            viewDialog.dismiss()
+        }
+
+        viewDialog.show()
+    }
+
+    private fun saveEditPost(editPost: PostsEntity){
+        editPost.save()
+        adapter?.notifyDataSetChanged()
+    }
+
+    private fun saveNewPost(message: String){
+        userLogged?.let {
+            val post = PostsEntity(it.name, message, Date(), it.id)
+            post.save()
+            listPosts.add(post)
+            adapter?.notifyDataSetChanged()
+        }
     }
 }
